2ffc27ef2577658504d373dfb22e7dc35734975c
Arpan Mishra <mishraarpan6@gmail.com>
2026-06-07 15:21:50 +0530

    Add: palindrome-permutation - submission-1

---
+++ Data Structures & Algorithms/palindrome-permutation/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPermutePalindrome(self, s: str) -> bool:
+        
+        #all characters have to be in paris except 1
+
+        char_count = Counter(s)
+        odd_count = 0
+
+        for t in char_count:
+            if char_count[t]%2!=0:
+                odd_count+=1
+            if odd_count>1:
+                return False
+
+        
+        return True
+
